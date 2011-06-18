7be75498e3145abd2cb8758d107af232b07525cc
Charles Finkel <charles.finkel@gmail.com>
2011-06-18 12:06:00 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Findmeapet::Application.config.secret_token = 'ab9634165edb2c226abe4e05f4117ed5b8f6bd1d98636d3d235fa97e7c2f69e3062cc1dd19a45c6b7448de07e8a2ef49e50ab37a5e314f7b69b4e4a54a945576'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Findmeapet::Application.config.session_store :cookie_store, :key => '_findmeapet_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Findmeapet::Application.config.session_store :active_record_store
